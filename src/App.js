8aa4066281dbf35a9112c9ad10fae6377d629457
zay <damilolaoyeniyi13@gmail.com>
2022-10-16 22:19:51 +0100

    bug fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import Die from './Die'
 import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App(){
   const [dice , setDice] = React.useState(allNewDice())
@@ -62,6 +63,7 @@ export default function App(){
 
   return(
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">
        Tenzies
       </h1>
